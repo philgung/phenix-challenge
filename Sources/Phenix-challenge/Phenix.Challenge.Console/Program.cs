e1bcc95b94bf7e325ca5c253208623e2d13ef0d2
Philippe Gung <philippe.gung@youscribe.com>
2019-05-17 10:41:40 +0200

    init repo

---
+++ Sources/Phenix-challenge/Phenix.Challenge.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Diagnostics;
+
+namespace Phenix.ChallengeConsole
+{
+    class Program
+    {
+        Action<Action> measures = impl =>
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            impl();
+            sw.Stop();
+            Console.WriteLine($"Temps écoulé : {sw.ElapsedMilliseconds} ms.");
+        };
+        static void Main(string[] args)
+        {
+            // Transaction : 
+            // txId|datetime|magasin|produit|qte
+            // 1 |20170514T223544+0100|2a4b6b81-5aa2-4ad8-8ba9-ae1a006e7d71|531|5
+
+            // Referenciel Produit
+            // produit|prix
+            // 1|4.7
+
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
