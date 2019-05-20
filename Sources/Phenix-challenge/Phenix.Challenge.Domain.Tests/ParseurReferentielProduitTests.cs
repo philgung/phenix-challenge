1b01137ee4dc31bb294ddecfd083271d2d2d862a
Philippe Gung <philippe.gung@youscribe.com>
2019-05-20 07:16:56 +0200

    Test ref produits

---
+++ Sources/Phenix-challenge/Phenix.Challenge.Domain.Tests/ParseurReferentielProduitTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Phenix.Challenge.Domain.Tests
+{
+    public class ParseurReferentielProduitTests
+    {
+        [Fact]
+        public void ParseReferentielProduit()
+        {
+            // Arrange
+
+            // Act
+            var referentielProduit = ParseurReferentielProduit.Parse(ligne)
+            // Assert
+
+        }
+    }
+}
