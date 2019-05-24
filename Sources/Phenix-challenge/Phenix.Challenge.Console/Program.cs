4e69791986b221ccea83450b8fbeb2d0d0696307
Philippe Gung <philippe.gung@youscribe.com>
2019-05-24 11:10:19 +0200

    benchmark temps d'exécution

--- Sources/Phenix-challenge/Phenix.Challenge.Console/Program.cs
+++ Sources/Phenix-challenge/Phenix.Challenge.Console/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using Phenix.Challenge.Domain;
+using Phenix.Challenge.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -7,7 +9,7 @@ namespace Phenix.ChallengeConsole
 {
     class Program
     {
-        Action<Action> measures = impl =>
+        static Action<Action> measures = impl =>
         {
             var sw = new Stopwatch();
             sw.Start();
@@ -18,14 +20,26 @@ namespace Phenix.ChallengeConsole
         static void Main(string[] args)
         {
             // création rapport
-
+            var rapport_14052017 = new RapportJournalier(new DateTime(2017, 05, 14), "../../../../../../data", new LecteurFichier());
             // remplissage rapport du dossier data
 
             // Génération rapports
+            measures(() => {
+                var meilleursVentes = rapport_14052017.Obtenir100MeilleursVentesEnGeneral();
+                Console.WriteLine("MeilleursVentes :");
+            });
+
+            measures(() =>
+            {
+                var plusGrosCA = rapport_14052017.Obtenir100PlusGrosChiffreDAffaireEnGeneral();
+                Console.WriteLine("Plus gros chiffre d'affaires :");
+            });
+
+
 
             // Export rapports en fichier.
-            
-            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
 
 
         }
