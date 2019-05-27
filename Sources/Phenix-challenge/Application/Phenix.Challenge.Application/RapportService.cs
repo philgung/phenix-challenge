269d1aa07bc4c1b8fb9baad01e1364debf2982df
Philippe Gung <philippe.gung@youscribe.com>
2019-05-27 07:26:43 +0200

    ajout version hebdo

---
+++ Sources/Phenix-challenge/Application/Phenix.Challenge.Application/RapportService.cs
@@ -0,0 +1,63 @@
+﻿using Phenix.Challenge.Domain;
+using Phenix.Challenge.Services;
+using System;
+using System.Collections.Generic;
+
+namespace Phenix.Challenge.Application
+{
+    public class RapportService
+    {
+        private readonly string dossierRacine;
+        private readonly ILecteurFichier lecteurFichier;
+        // TODO : Cache sur rapport
+
+        public RapportService(string dossierRacine, ILecteurFichier lecteurFichier)
+        {
+            this.dossierRacine = dossierRacine;
+            this.lecteurFichier = lecteurFichier;
+        }
+
+        public IEnumerable<(int ProduitId, int QuantiteTotal)> Obtenir100MeilleursVentesJournaliereEnGeneral(DateTime dateDuJour)
+        {
+            var rapport = new Rapport(dateDuJour, dateDuJour, dossierRacine, lecteurFichier);
+            return rapport.Obtenir100MeilleursVentesEnGeneral();
+        }
+        public IEnumerable<(int ProduitId, decimal ChiffreDAffaire)> Obtenir100PlusGrosChiffreDAffaireJournaliereEnGeneral(DateTime dateDuJour)
+        {
+            var rapport = new Rapport(dateDuJour, dateDuJour, dossierRacine, lecteurFichier);
+            return rapport.Obtenir100PlusGrosChiffreDAffaireEnGeneral();
+        }
+        public IEnumerable<(int ProduitId, int QuantiteTotal)> Obtenir100MeilleursVentesJournaliereParMagasin(DateTime dateDuJour, Guid magasin)
+        {
+            var rapport = new Rapport(dateDuJour, dateDuJour, dossierRacine, lecteurFichier);
+            return rapport.Obtenir100MeilleursVentesParMagasin(magasin);
+        }
+        public IEnumerable<(int ProduitId, decimal ChiffreDAffaire)> Obtenir100PlusGrosChiffreDAffaireJournaliereParMagasin(DateTime dateDuJour, Guid magasin)
+        {
+            var rapport = new Rapport(dateDuJour, dateDuJour, dossierRacine, lecteurFichier);
+            return rapport.Obtenir100PlusGrosChiffreDAffaireParMagasin(magasin);
+        }
+
+        public IEnumerable<(int ProduitId, int QuantiteTotal)> Obtenir100MeilleursVentesHebdomadaireEnGeneral(DateTime dateDuJour)
+        {
+            var rapport = new Rapport(dateDuJour.AddDays(-7), dateDuJour, dossierRacine, lecteurFichier);
+            return rapport.Obtenir100MeilleursVentesEnGeneral();
+        }
+        public IEnumerable<(int ProduitId, decimal ChiffreDAffaire)> Obtenir100PlusGrosChiffreDAffaireHebdomadaireEnGeneral(DateTime dateDuJour)
+        {
+            var rapport = new Rapport(dateDuJour.AddDays(-7), dateDuJour, dossierRacine, lecteurFichier);
+            return rapport.Obtenir100PlusGrosChiffreDAffaireEnGeneral();
+        }
+        public IEnumerable<(int ProduitId, int QuantiteTotal)> Obtenir100MeilleursVentesHebdomadaireParMagasin(DateTime dateDuJour, Guid magasin)
+        {
+            var rapport = new Rapport(dateDuJour.AddDays(-7), dateDuJour, dossierRacine, lecteurFichier);
+            return rapport.Obtenir100MeilleursVentesParMagasin(magasin);
+        }
+        public IEnumerable<(int ProduitId, decimal ChiffreDAffaire)> Obtenir100PlusGrosChiffreDAffaireHebdomadaireParMagasin(DateTime dateDuJour, Guid magasin)
+        {
+            var rapport = new Rapport(dateDuJour.AddDays(-7), dateDuJour, dossierRacine, lecteurFichier);
+            return rapport.Obtenir100PlusGrosChiffreDAffaireParMagasin(magasin);
+        }
+
+    }
+}
